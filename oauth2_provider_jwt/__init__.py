e569b98d736da36b18ea975363ef0c85fb8c0097
jason <jason@zzq.org>
2021-06-29 10:15:09 -0600

    remove warning for defining default_app_config in django 3.2

--- oauth2_provider_jwt/__init__.py
+++ oauth2_provider_jwt/__init__.py
@@ -1,1 +1,8 @@
-default_app_config = "oauth2_provider_jwt.apps.AppConfig"
+import django
+
+
+major, minor, _ = (int(i) for i in django.__version__.split('.'))
+
+if major < 3 or (major == 3 and minor < 2):
+    # this is deprecated in 3.2 and removed in 4.1
+    default_app_config = "oauth2_provider_jwt.apps.AppConfig"
